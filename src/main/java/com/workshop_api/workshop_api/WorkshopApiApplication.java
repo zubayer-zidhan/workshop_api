87b4b045618ad3d80a1d1790a766c501a955561a
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-05-01 21:48:08 +0530

    first commit

---
+++ src/main/java/com/workshop_api/workshop_api/WorkshopApiApplication.java
@@ -0,0 +1,13 @@
+package com.workshop_api.workshop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workshop_api/workshop_api/WorkshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop_api.workshop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
