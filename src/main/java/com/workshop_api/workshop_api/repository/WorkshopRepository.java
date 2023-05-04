94a12781d09308d29459fe4fb9b82629558cb80b
Anurag Sahu <anishsahu33@gmail.com>
2023-05-04 23:13:02 +0530

    booking

--- src/main/java/com/workshop_api/workshop_api/repository/WorkshopRepository.java
+++ src/main/java/com/workshop_api/workshop_api/repository/WorkshopRepository.java
@@ -1,11 +1,9 @@
 package com.workshop_api.workshop_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.workshop_api.workshop_api.entities.Workshops;
 
-@Repository
+
 public interface WorkshopRepository extends JpaRepository<Workshops, Integer>{
     
 }
