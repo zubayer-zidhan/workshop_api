48f10f0f5e3202e1ce7cdf94119dbb39bfc28f71
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-05-07 02:03:22 +0530

    Added available-slots route

--- src/main/java/com/workshop_api/workshop_api/services/SlotsAvailabilityService.java
+++ src/main/java/com/workshop_api/workshop_api/services/SlotsAvailabilityService.java
@@ -9,4 +9,6 @@ import com.workshop_api.workshop_api.entities.SlotsAvailability;
 @Service
 public interface SlotsAvailabilityService {
    public List<SlotsAvailability> getSlotsAvailabilityDetails(); 
+   public String getAvailableSlotsInWorkshop(int wid, String bdate);
+   public String getAvailableSlotsInCity(int cid, String bdate);
 }
