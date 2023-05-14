6a74605552fe5141e129de975cfdee3b9ce6cbd5
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-05-14 21:38:37 +0530

    Added COORS to cities

--- src/main/java/com/workshop_api/workshop_api/controller/CitiesController.java
+++ src/main/java/com/workshop_api/workshop_api/controller/CitiesController.java
@@ -3,6 +3,7 @@ package com.workshop_api.workshop_api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,7 @@ public class CitiesController {
     @Autowired
     CitiesService citiesService;
     
+    @CrossOrigin(value = "http://localhost:3000")
     @GetMapping("/getCities")
     public List<Cities> getAllCities(){
         return citiesService.getAllCities();
