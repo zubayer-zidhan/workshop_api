7716c7fe7efd30beaf906352fabc4c6315f4bb9c
Anurag Sahu <anishsahu33@gmail.com>
2023-05-14 20:46:09 +0530

    added crossorigin workshop

--- src/main/java/com/workshop_api/workshop_api/controller/WorkshopController.java
+++ src/main/java/com/workshop_api/workshop_api/controller/WorkshopController.java
@@ -1,6 +1,7 @@
 package com.workshop_api.workshop_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +11,7 @@ import com.workshop_api.workshop_api.services.WorkshopService;
 
 import com.workshop_api.workshop_api.entities.Workshops;
 import java.util.List;
-
+@CrossOrigin(value = "http://localhost:3000")
 @RestController
 public class WorkshopController {
     
